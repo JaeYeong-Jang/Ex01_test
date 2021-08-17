82197c988284855f2051184b4cbcbafe3b6fd65a
jangjaeyeong <xkxh96@gmail.com>
2021-08-17 17:31:27 +0900

    08_16Class

---
+++ app/src/main/java/com/javaex/ex01/MainActivity.java
@@ -0,0 +1,22 @@
+package com.javaex.ex01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //fields
+    
+    //constructors
+
+    //g/s
+
+    //method
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
